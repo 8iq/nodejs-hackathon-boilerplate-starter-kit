1568682f898b86102c1917249a1e71ab51e2634a
Дмитрий Данев <Danewdima@gmail.com>
2020-09-13 02:35:09 +0300

    chat_implementation: minor update

--- apps/_compability_app/servers/socket.js
+++ apps/_compability_app/servers/socket.js
@@ -1,29 +1,40 @@
 const server = require("http").createServer();
 
-const io = require("socket.io")(server, {
-    path: "/",
-    serveClient: false,
-    // below are engine.IO options
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false
-});
+const io = require("socket.io");
 
 server.listen(8000);
 
-function initSocketServer() {
-    io.on("connection", (socket) => {
-        console.log(socket.id, "a user connected");
-
-        socket.on("chat message", (msg) => {
-            console.log("message", socket.id, msg);
-            io.emit("chat message", msg)
-        });
-
-        socket.on("disconnect", () => {
-            console.log(socket.id, 'user disconnected')
+class SocketServer {
+    constructor() {
+        this.socket_server = io(server, {
+            path: "/",
+            serveClient: false,
+            pingInterval: 10000,
+            pingTimeout: 5000,
+            cookie: false
         })
-    })
+    }
+
+    init() {
+        this.socket_server.on("connection", (socket) => {
+            console.log(socket.id, "a user connected");
+
+            socket.on("chat message", (msg) => {
+                console.log("message", socket.id, msg);
+                io.emit("chat message", msg)
+            });
+
+            socket.on("disconnect", () => {
+                console.log(socket.id, 'user disconnected')
+            })
+        })
+    }
+
+    socket_server = null;
+}
+
+function initSocketServer() {
+    new SocketServer();
 }
 
 module.exports = {
